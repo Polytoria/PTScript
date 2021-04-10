39230481db685a82029b8305b17fef2d39faa55b
Sylack <ilarumbgim1@educacion.navarra.es>
2021-04-10 11:45:43 +0000

    V!

--- Users/index.ts
+++ Users/index.ts
@@ -1,16 +1,36 @@
 import axios from "axios";
 
 class User {
-    public token;
-    constructor(csrf:string){
-        this.token = csrf;
+    public id:number;
+    constructor(id){
+        this.id = id;
     }
-    public async block(id){
-        const promise = axios.post('https://polytoria.com/api/users/block',{
-            csrf: this.token,
-            id: id
-        })
-        .then((response) => response.data)
-        .catch((err) => console.log(err));
+    public async getUserData(){
+        const promise = axios.get(`https://api.polytoria.com/users/user?id=${this.id}`);
+        const promiseData : Object = promise.then((Response)=>Response.data)
+        return promiseData;
+    }
+    /**
+     * @type Object
+     * @var this.id ID variable.
+     * @returns JSON of Friends.
+     */
+    public async getFriends(){
+        const promise = axios.get(`https://api.polytoria.com/users/friends?id=${this.id}`);
+        const promiseData : Object = promise.then((Response)=>Response.data)
+        return promiseData;
+    }
+    public async getAppreance(){
+        const promise = axios.get(`https://api.polytoria.com/users/getappearance?id=${this.id}`);
+        const promiseData : Object = promise.then((Response)=>Response.data)
+        return promiseData;
+    }
+    public async getUsername(){
+
+    }
+    public static async getFromUsername(username:string){
+        const promise = axios.get(`https://api.polytoria.com/users/getbyusername?username=${username}`);
+        const promiseData : Object = promise.then((Response)=>Response.data)
+        return promiseData;
     }
 }
